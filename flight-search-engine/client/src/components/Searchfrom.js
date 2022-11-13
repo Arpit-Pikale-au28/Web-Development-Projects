a7076c159ebb6e8bd70220b8231028d4a2219881
arpitpikale <arpitpikle96@gmail.com>
2022-11-13 11:01:07 +0530

    updated files

--- flight-search-engine/client/src/components/Searchfrom.js
+++ flight-search-engine/client/src/components/Searchfrom.js
@@ -1,12 +1,14 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import '../App.css'
 
 export default function Searchfrom() {
   var [originCity, setoriginCity] = useState('')
   var [destinationCity, setdestinationCity] = useState('')
   var [departureDate, setdepartureDate] = useState('')
+  var [data, setData] = useState([])
 
-  const submitHandler = (event) => {
+    const submitHandler = (event) => {
     event.preventDefault();
     axios.post('http://localhost:8000/postData',{
       originCity,
@@ -15,20 +17,48 @@ export default function Searchfrom() {
     })
     .then((response) => {
       console.log(response);
+      setData(response.data)
       setoriginCity('')
       setdestinationCity('')
       setdepartureDate('')
     })
     .catch((error) => console.log(error));
   }
+   const getFlights = ()=> {
+       data.map((flight)=> {
+        const {id, name, price, originCity, destinationCity, departureDate, departureTime} = flight;
+        return (
+                <div id='flights' key={id}>
+                     <h3>Price &#x20b9; {price.toFixed(2)}</h3>
+                     <p> {name} </p>
+                     <p>origin {'>>'} {originCity}</p>
+                     <p>destination {'>>'} {destinationCity}</p>
+                     <p>departureDate {'>>'} {departureDate}</p>
+                     <p>departureTime {'>>'} {departureTime}</p>
+                </div>
+        )
+       })
+   }
+
+    useEffect(()=> {
+      if(data.length !== 0) {
+        getFlights()
+      }
+    }, [])
+
   return (
+    <>
     <div  className="form-group" id='myform' onSubmit={submitHandler}>
         <form>
-            <input type="text" className="form-control" style={{width: "300px", margin:"15px"}} placeholder='Enter origin city'value={originCity} onChange={(event) => setoriginCity(event.target.value)}/>
-            <input type="text" className="form-control"  style={{width: "300px",  margin:"15px"}} placeholder='Enter destination city' value={destinationCity} onChange={(event) => setdestinationCity(event.target.value)}/>
-            <input type="date" className="form-control"style={{width: "300px",  margin:"15px"}} value={departureDate} onChange={(event) => setdepartureDate(event.target.value)}/>
+            <input type="text" className="form-control" style={{width: "300px", margin:"15px"}} placeholder='Enter origin city' required value={originCity} onChange={(event) => setoriginCity(event.target.value)}/>
+            <input type="text" className="form-control"  style={{width: "300px",  margin:"15px"}} placeholder='Enter destination city' required value={destinationCity} onChange={(event) => setdestinationCity(event.target.value)}/>
+            <input type="date" className="form-control"style={{width: "300px",  margin:"15px"}} value={departureDate} required onChange={(event) => setdepartureDate(event.target.value)}/>
             <button type="submit" className="btn btn-primary" style={{width: "300px",  margin:"15px"}} >Submit</button>
         </form>
     </div>
+    <div id='getflights'>
+       <>{getFlights}</> 
+    </div>
+    </>
   )
 }
--- flight-search-engine/server/app.js
+++ flight-search-engine/server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const path = require('path')
 const fs = require('fs')
 const jsonFile = require('./assignment-data.json');
+const { isTypedArray } = require('util/types')
 
 
 app.use(cors())
@@ -19,19 +20,33 @@ app.post("/postData", (req, res) => {
     let string = req.body;
     let originCity = string.originCity.charAt(0).toUpperCase() + string.originCity.slice(1);
     let destinationCity = string.destinationCity.charAt(0).toUpperCase() + string.destinationCity.slice(1);
-    let departureDate = string.departureDate
-    departureDate = departureDate.replace(/\-/g, '/')
-    jsonFile.map((item) => {
-        if(item.originCity === originCity && item.destinationCity === destinationCity){
-            console.log(item);
-        }
-        else{
-            console.log("flight not found");
+    // let val = string.departureDate;
+    // const myArray = val.split("-");
+    // let year = myArray[0];
+    // let month = myArray[1];
+    // let day = myArray[2];
+    // if(month.charAt(0) == "0"){
+    //     month = month.charAt(1)
+    // }
+    // if(day.charAt(0) == '0'){
+    //     day = day.charAt(1)
+    // }
+    // let formatteddate = day + "/" + month + "/" + year; 
+    let arr = []
+    jsonFile.results.map((item) => {
+        if(item.originCity === originCity && item.destinationCity === destinationCity) {
+            arr.push(item)
         }
     })
+    if(arr.length == 0){
+        res.send({err:"flight not found"})
+    }
+    else{
+        res.send(arr)
+    }
 })
-
 app.listen(8000, ()=> {
     console.log("Server is running at port 3000");
     console.log("http://localhost:8000");
 })
+
