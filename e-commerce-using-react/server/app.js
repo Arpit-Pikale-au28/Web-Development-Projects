07b64286f7d6ee28e3c21903c61867c01537f306
arpitpikale <arpitpikle96@gmail.com>
2023-01-05 11:21:29 +0530

    updated server

--- e-commerce-using-react/server/app.js
+++ e-commerce-using-react/server/app.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 const bcrypt = require('bcrypt');
 
 // setUp mongodb server connection
-const url = process.env.MONGO_URL
+const url =  process.env.MONGO_URL
 const client = new MongoClient(url)
 var collection = null
 
@@ -25,34 +25,49 @@ app.use(express.json())
 app.post('/register', async(req, res) => {
     const {name, email, mobile,password, cpassword} = await req.body
     let encryptPassword =  await bcrypt.hash(password, 10)
+    let oldUser = null
     try {
-        let oldUser = await collection.find({email:email}).toArray()
-        if(oldUser[0].email === email){
-        return res.send({err:"user already Exists"})
-        }
-    
-        await collection.insertOne({name, email, mobile, password: encryptPassword})
-        res.send({status:"ok"})
-    }
+        oldUser = await collection.find({email:email}).toArray()
+       }
     catch(err) {
-         res.send({status: "error"})
+         res.send({status: "Something goes wrong try again!!"})
     }
-    
+    if (oldUser.length != 0 && oldUser[0].email === email){
+          return res.send({status:"User Already Exists"})
+    }
+    else {
+            await collection.insertOne({name, email, mobile, password: encryptPassword})
+            res.send({status:"User sucessfully registered"})
+        }
 })
+    
 
 app.post('/login', async(req,res)=> {
     const {email, password} = await req.body
+    let findUser = null
     console.log(req.body);
-    let findUser = await collection.find({email: email}).toArray()
-    let mongoEmail = await findUser[0].email
-    let mongoPassword = await findUser[0].password
-    if(email === mongoEmail && password === mongoPassword){
-        console.log("login credentials are correct");
-        res.send(findUser)
+    try {
+        findUser = await collection.find({email:email}).toArray()
+    }
+    catch(err){
+        //console.log(err)
+        return res.send({status:"User not registered"})
+    }
+    if (findUser.length != null){
+        let mongoEmail = await findUser[0].email
+        let mongoPassword = await findUser[0].password
+        const passwordMatch = await bcrypt.compare(password, mongoPassword)
+        console.log(passwordMatch)
+    if(email === mongoEmail && passwordMatch === true){
+       // console.log("login credentials are correct");  
+        res.send({status:"login credentials are correct"})
     }
     else{
-        console.log("inccorrect username/password");
+       // console.log("inccorrect username/password");
+        res.send({status:"inccorrect username/password"})
     }
+    }
+    
 })
 
 app.listen(5000, ()=> {
